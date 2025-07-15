fcd7ba370013d57e0b97508f9cda3142c064fa27
rajatkaran <70325648+rajatkaran@users.noreply.github.com>
2025-07-15 23:32:12 +0530

    Update index.jsx

--- website/index.jsx
+++ website/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app"; // ✅ Yeh tera current app.jsx
+import App from "./src/app"; // ✅ Yeh tera current app.jsx
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
