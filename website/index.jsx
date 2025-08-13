be680152b27dcc9e4283967ca2cb9cfe025bebc3
rajatkaran <70325648+rajatkaran@users.noreply.github.com>
2025-08-13 18:00:49 +0530

    Update index.jsx

--- website/index.jsx
+++ website/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../src/app.jsx'; // go up one level, then into src
+import App from './src/app.jsx'; // go up one level, then into src
 import './index.css'; // this CSS file will be in utils/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
