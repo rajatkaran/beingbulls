ca42cde6661f03d88795a3d52fbdf4348696b72e
rajatkaran <70325648+rajatkaran@users.noreply.github.com>
2025-12-01 18:21:49 +0530

    Update app.jsx

--- website/src/app.jsx
+++ website/src/app.jsx
@@ -3,17 +3,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "../index.css"; // global tailwind + styles
+import "../index.css";
 
-import Navbar from "../components/navbar.jsx"; // your navbar file
+import Navbar from "../components/navbar.jsx";
 import Home from "../pages/Home.jsx";
 import Login from "../pages/login.jsx";
 import Signup from "../pages/signup.jsx";
 import Subscribe from "../pages/subscribe.jsx";
 import Dashboard from "../pages/dashboard.jsx";
 import ForgotPassword from "../pages/forgot-password.jsx";
+import PrivateRoute from "../components/PrivateRoute.jsx";
 
-function App() {
+function App(){
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col">
@@ -24,9 +25,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/subscribe" element={<Subscribe />} />
-            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
-            {/* fallback */}
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
             <Route path="*" element={<Home />} />
           </Routes>
         </main>
@@ -35,6 +35,5 @@ function App() {
   );
 }
 
-const container = document.getElementById("root");
-createRoot(container).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
 export default App;
